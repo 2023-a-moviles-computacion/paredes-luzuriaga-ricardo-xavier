c63fcf45c5623885464a6bd06dd0d3954876444b
Ricardo <ricardo.paredes@epn.edu.ec>
2023-07-03 11:53:45 -0500

    Commit N#11 03/07/2023
    Se implementó el CRUD

--- Proyectos/01 - Android - Profe/app/src/main/java/com/example/movilescomputacion2023a/MainActivity.kt
+++ Proyectos/01 - Android - Profe/app/src/main/java/com/example/movilescomputacion2023a/MainActivity.kt
@@ -1,9 +1,13 @@
 package com.example.movilescomputacion2023a
 
+import android.app.Activity
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.ContactsContract
 import android.widget.Button
+import androidx.activity.result.contract.ActivityResultContracts
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,12 +26,82 @@ class MainActivity : AppCompatActivity() {
                 irActividad(BListView::class.java)
             }
 
-    }
 
-    fun irActividad(
-    clase: Class<*>
-    ){
-        val intent = Intent(this, clase)
-        startActivity(intent)
+        val callbackIntentPickUri = registerForActivityResult(
+            ActivityResultContracts.StartActivityForResult()
+        ) { result ->
+            if (result.resultCode === RESULT_OK)
+                if (result.data != null) {
+                    if (result.data!!.data != null) {
+                        val uri: Uri = result.data!!.data!!
+                        val cursor = contentResolver.query(uri, null, null, null, null, null)
+                        cursor?.moveToFirst()
+                        val indiceTelefono = cursor?.getColumnIndex(
+                            ContactsContract.CommonDataKinds.Phone.NUMBER
+                        )
+                        val telefono = cursor?.getString(indiceTelefono!!)
+                        cursor?.close()
+                        "Telefono: ${telefono}"
+                    }
+                }
+        }
+        // Implícito
+        val botonIntentImplicito = findViewById<Button>(R.id.btn_ir_intent_implicito)
+        botonIntentImplicito.setOnClickListener {
+            val intentConRespuesta = Intent(
+                Intent.ACTION_PICK,
+                ContactsContract.CommonDataKinds.Phone.CONTENT_URI
+            )
+            callbackIntentPickUri.launch(intentConRespuesta)
+        }
+        //Explícito
+        val botonIntentExplicito = findViewById<Button>(R.id.btn_ir_intent_explicito)
+        botonIntentExplicito.setOnClickListener {
+            abrirActividadConParametros(CIntentExplicitoParametros::class.java)
+        }
     }
+    val callbackContenidoCIntentExplicito =
+        registerForActivityResult(
+            ActivityResultContracts.StartActivityForResult()
+        ) { result ->
+            if (result.resultCode == Activity.RESULT_OK) {
+                if (result.data != null) {
+                    //Lógica de Negocio
+                    val data = result.data
+                    "${data?.getStringExtra("NombreModificado")}"
+                }
+            }
+        }
+
+        fun abrirActividadConParametros(
+            clase: Class<*>
+        ) {
+            val intentExplicito = Intent(this, clase)
+            //Enviar parámetros (Solamente variables primitivas)
+            intentExplicito.putExtra("nombre", "Ricardo")
+            intentExplicito.putExtra("apellido", "Paredes")
+            intentExplicito.putExtra("edad", 21)
+            val boton = findViewById<Button>(R.id.btn_devolver_respuesta)
+            boton.setOnClickListener { devolverRespuesta() }
+            //Este call back no funcionó xd
+            //Este call back ya funcionó xd
+            callbackContenidoCIntentExplicito.launch(intentExplicito)
+        }
+        fun devolverRespuesta(){
+            val intentDevolverParametros = Intent()
+            intentDevolverParametros.putExtra("nombreModificado", "Nahomy")
+            intentDevolverParametros.putExtra("edadModificado", 20)
+            setResult(
+                RESULT_OK,
+                intentDevolverParametros
+            )
+            finish()
+        }
+
+        fun irActividad(
+            clase: Class<*>
+        ) {
+            val intent = Intent(this, clase)
+            startActivity(intent)
+        }
 }
