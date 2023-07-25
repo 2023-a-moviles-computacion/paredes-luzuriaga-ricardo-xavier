d7961161b442f3a4faf83296d297c5b3b2579e9c
Ricardo <ricardo.paredes@epn.edu.ec>
2023-07-25 10:20:09 -0500

    Commit N#19 25/07/2023
    Se logró completar los mapas, y también se completó la interacción entre las ubicaciones aunque no lo pude desplegar por la VDI

--- Proyectos/01 - Android - Profe V2/app/src/main/java/com/example/movilescomputacion2023a/GGoogleMapsActivity.kt
+++ Proyectos/01 - Android - Profe V2/app/src/main/java/com/example/movilescomputacion2023a/GGoogleMapsActivity.kt
@@ -3,19 +3,35 @@ package com.example.movilescomputacion2023a
 import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.*
 
 class GGoogleMapsActivity : AppCompatActivity() {
     private lateinit var mapa: GoogleMap
     var permisos = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_ggoogle_maps)
         solicitarPermisos()
         iniciarLogicaMapa()
+        val boton = findViewById<Button>(R.id.btn_ir_carolina)
+        boton.
+                setOnClickListener {
+                    irCarolina()
+                }
+    }
+
+    fun irCarolina(){
+        val carolina = LatLng(-0.185684318486696,-78.48447277600916)
+        val zoom=17f
+        moverCamaraConZoom(carolina,zoom)
     }
 
     fun solicitarPermisos(){
@@ -50,6 +66,10 @@ class GGoogleMapsActivity : AppCompatActivity() {
             with(googleMap){
                 mapa = googleMap
                 establecerConfiguracionMapa()
+                moverQuicentro()
+                anadirPolilinea()
+                anadirPoligono()
+                escucharListeners()
             }
         }
     }
@@ -70,4 +90,84 @@ class GGoogleMapsActivity : AppCompatActivity() {
             uiSettings.isZoomControlsEnabled = true //Esto va por defecto
         }
     }
+    fun anadirMarcador(latLng: LatLng, title:String): Marker {
+        return mapa.addMarker(
+            MarkerOptions()
+                .position(latLng)
+                .title(title)
+        )!!
+    }
+    fun moverCamaraConZoom(latLng: LatLng, zoom:Float = 10f){
+        mapa.moveCamera(
+            CameraUpdateFactory
+                .newLatLngZoom(latLng,zoom)
+        )
+    }
+    fun moverQuicentro(){
+        val zoom = 17f
+        val quicentro = LatLng(
+            -0.175567084902771092, -78.48014901143776
+        )
+        val titulo = "Quicentro"
+        val markQuicentro = anadirMarcador(quicentro, titulo)
+        markQuicentro.tag = titulo
+        moverCamaraConZoom(quicentro, zoom)
+    }
+    fun anadirPolilinea(){
+        with(mapa){
+            val poliLineaUno = mapa
+                .addPolyline(
+                    PolylineOptions()
+                        .clickable(true)
+                        .add(
+                            LatLng(-0.1759187040647396,-78.48506472421384),
+                            LatLng(-0.17632468492901104,-78.48265589308046),
+                            LatLng(-0.17746143130181483, -78.4770533307815)
+                        )
+                )
+            poliLineaUno.tag = "Linea-1"
+        }
+    }
+    fun anadirPoligono(){
+        with(mapa){
+            val poligonoUno = mapa.addPolygon(
+                PolygonOptions()
+                    .clickable(true)
+                    .add(
+                        LatLng(-0.1771546902239471, -78.48344981495214),
+                        LatLng(-0.1796891486125768, -78.48269198043828),
+                        LatLng(-0.17710958124147777, -78.48142892291516)
+                    )
+            )
+            poligonoUno.fillColor = -0xc771c4
+            poligonoUno.tag = "poligono-2"
+        }
+    }
+    fun escucharListeners(){
+        mapa.setOnPolygonClickListener {
+            Log.i("mapa", "setOnPolygonClickListener ${it}")
+            it.tag // ID
+        }
+        mapa.setOnPolylineClickListener {
+            Log.i("mapa", "setOnPolylineClickListener ${it}")
+            it.tag // ID
+        }
+        mapa.setOnMarkerClickListener {
+            Log.i("mapa", "setOnMarkerClickListener ${it}")
+            it.tag // ID
+            return@setOnMarkerClickListener true
+        }
+        mapa.setOnCameraMoveListener {
+            Log.i("mapa", "setOnCameraMoveListener")
+        }
+        mapa.setOnCameraMoveStartedListener {
+            Log.i("mapa", "setOnCameraMoveStartedListener ${it}")
+        }
+        mapa.setOnCameraIdleListener {
+            Log.i("mapa", "setOnCameraIdleListener")
+        }
+    }
+
+
+
 }
